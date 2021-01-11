cdb8b8a55eb1391bde482cd550d8fe7d8a039f96
dyd999x <idiana96@yahoo.com>
2021-01-11 15:06:28 +0000

    updated check

---
+++ Verifier.py
@@ -0,0 +1,33 @@
+
+from CheckFormat import checkFormat
+import numpy as np
+
+def verify(T, gamma, dict1, dict2, proof):
+    (m, n) = gamma.shape
+    t1 = list(dict1.keys())
+    t2 = list(dict2.keys())
+    C = np.array(list(dict1.values()))
+    D = np.array(list(dict2.values()))
+    D = D.T
+    (dim1_C, dim2_C) = C.shape
+    (dim1_D, dim2_D) = D.shape
+
+    p1 = np.array(proof[1])
+    p2 = np.array(proof[2])
+    p3 = np.array(proof[3])
+    p4 = np.array(proof[4])
+
+    (dim1_p1, dim2_p1) = p1.shape
+    (dim1_p2, dim2_p2) = p2.shape
+    (dim1_p3, dim2_p3) = p3.shape
+    (dim1_p4, dim2_p4) = p4.shape
+
+    if checkFormat(T, gamma, t1, t2):
+        if (dim1_C == 2 and dim2_C == m) and (dim1_D == n and dim2_D == 2):
+            if dim1_p1 ==2 and dim2_p1 == 1 and dim1_p2 == 2 and dim2_p2 == 1:
+                return True
+
+
+
+
+
